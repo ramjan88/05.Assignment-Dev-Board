654742c0f112d95b52741f1904d4da05c9a3803c
ramjan88 <gmramjanali8888@gmail.com>
2025-03-01 13:54:48 -0600

    added update time part

--- js/dev-board.js
+++ js/dev-board.js
@@ -3,9 +3,14 @@
 document.getElementById("theme-btn").addEventListener("click", function () {
   randomBackgroundColor();
 });
-
 //
 // click and visite the blog site
 document.getElementById("discoverd-box").addEventListener("click", function () {
   window.location.href = "blog.html";
 });
+
+//show current date
+let date =new Date()
+document.getElementById("curernt-date").innerHTML=date.toDateString();
+
+// card- 1
